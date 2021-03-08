871c6149d133771be937527d9e8e0d2c420b65d5
Calvet Romain <metro5330@gmail.com>
2021-03-08 21:11:16 +0100

    changing index.html to index.php + delete index.php

--- index.php
+++ index.php
@@ -1,3 +1,123 @@
-<?php
-    include "index.html";
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" type="image/x-png" href="./img/logo.png" />
+    <title>Escalanque</title>
+</head>
+<body class="container-fluid">
+    <div class="row" id="header">
+        <div class="col-12 align-middle" style="height: 100%;">
+            <div class="row justify-content-center" style="height: 50%;">
+                <div class="col-10 display-1 title align-items-center"><span class="shadow">ESCALANQUE</span></div>
+            </div>
+            <div class="row justify-content-center" style="height: 50%;">
+                <div class="col-12 display-6 subtitle align-items-center"><span class="shadow">Association d'escalade dans les calanques de Marseille</span></div>
+            </div>
+        </div>
+    </div>
+    <div class="row sticky-top" id="menu">
+        <nav class="nav">
+            <a class="nav-link active" aria-current="page" href="#">Acceuil</a>
+            <a class="nav-link" href="./compet.html">La compet'</a>
+          </nav>
+    </div>
+    <div class="container-fluid" id="body">
+        <div class="row m-4" id="intro">
+            <div class="col-12">
+                <p>Pour le plaisir des sens vous découvrirez le rocher et l'ambiance unique des Calanques. Que vous soyez débutant ou expérimenté, On vous propose un encadrement personnalisé sur les plus belles falaises de la région.</p>
+                <p>Découvrez nos cours et stages d’escalade pour enfants, ados et adultes. Entrainement et participation aux compétitions. Encadrement de groupes, collectivités, scolaires, etc… Engagement à la journée ou 1/2 journée pour cours particuliers, grandes voies… En extérieur dans le Parc National des Calanques de Marseille à Cassis.</p>
+            </div>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4 align-items-center" id="pics">
+            <div class="col">
+                <div class="card">
+                <img src="./img/1.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">L'escalade en famille</h5>
+                    <p class="card-text">L’escalade est une très bonne activité pour faire gagner de la confiance en eux aux enfants. Ils ont un peu peur et doivent surmonter leur crainte. Après avoir réussi, ils sont fiers d’eux et cela leur fait gagner en confiance.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="./img/2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Des voies extrêmes</h5>
+                    <p class="card-text">Colline de Lun - deux secteurs distincts dont un bien abrité du mistral - celui de droite qui est le plus récent offre de très belles longueurs verticales sur du beau rocher à gouttes d'eau - celui de gauche propose quelques cotations plus difficiles.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="./img/3.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Pourquoi les calanques ?</h5>
+                    <p class="card-text">Le choix dans les calanques est très varié avec des grandes classiques comme les courses d'arête (arête de Marseille, arête de Cassis, arête du Vallon...), les grandes traversées (la sans retour, la grande croisière, la bora bora, la commune...), les voies historiques (la civa, le temple, les futurs croulants...)</p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="./img/4.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Initiation</h5>
+                    <p class="card-text">Pour le plaisir des sens vous découvrirez le rocher et l'ambiance unique des Calanques. Que vous soyez débutant ou expérimenté, je vous propose un encadrement personnalisé sur les plus belles falaises de la région</p>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 g-4 m-2 align-items-center" id="articles">
+            <div class="col">
+                <div class="card" id="article-1">
+                    <div class="card-body">
+                        <h5 class="card-title">Aller a l'Escalier des Géants</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Retrouvez les voies ainsi que les accès a l'Escalier des Géants</h6>
+                        <p class="card-text">Petit site en 3 parties :<br>
+                            - à gauche un mur vertical (bien équipées),<br>
+                            - à droite de courtes voies bloc avec surplomb au départ (équipement ancien),<br>
+                            - au fond quelques couennes en dièdre (bien équipées).</p>
+                        <p class="card-text">Le site se situe sur le chemin conduisant au Rocher St Michel, juste à l'entrée du défilé de l'Escalier des Géants.
+                            Suivre la rue qui prolonge la Calanque de Callelongue, et continuer sur un chemin jusqu'à une maison isolée.
+                            Franchir une barrière, laisser à gauche une carrière abritant un récent petit site de grimpe.
+                            Après 70 m, à un croisement, continuer au NE en direction de la grande falaise du Rocher St Michel.
+                            200 m plus loin, à un nouveau croisement, poursuivre droit devant au N pendant 120 m jusqu'à un cirque minuscule.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" id="article-2">
+                    <div class="card-body">
+                      <h5 class="card-title">Après l'effort le réconfort. </h5>
+                      <h6 class="card-subtitle mb-2 text-muted">Ou manger, boire un verre les pieds dans l'eau.</h6>
+                      <p class="card-text">Aux portes des calanques, situé aux Goudes, village de pêcheurs au sud de Marseille dans le 8éme arrondissement, le bar 20 000 Lieues crée il y a plus de 16 années vous surprendra avec sa décoration intérieure, les détails tout droit sortis de l'univers de Jules Verne et ses fenêtres telles des hublots vous donneront l'impression d'être en pleine mer.</p>
+                      <p class="card-text">Le Château propose de véritables délices, en fonction des arrivages : lasagnes de Saint-jacques aux poireaux et coquillages, filet de rouget soleillade ou encore brochette de lotte au basilic. Les viandards et les végétariens y trouveront aussi leur compte, emplissant leurs assiettes de bons produits ensoleillés.</p>
+                      <p class="card-text">On accède au Nautic Bar en voiture (selon les réglementations de la saison), à pied, après une bonne heure de marche ou en bâteau, il faut mériter son petit coin de paradis ! On ne sait pas si c’est la vue sur la mer, les petits cabanons du port de Morgiou ou la carte de poissons (et bouillabaisse !), mais on se croirait en vacances…</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row row-cols-1 row-cols-sm-3" id="footer">
+            <div class="col align-items-center flex-col">
+                <a href="mailto:asso@Escalanque.fr">asso@Escalanque.fr</a>
+                <a href="tel:0494000013">04 94 00 00 13</a>
+                <span class="empty"></span>
+                <span>Inscription pour la compet':</span>
+                <a href="mailto:lacompet@Escalanque.fr">lacompet@Escalanque.fr</a>
+            </div>
+            <div class="col align-items-center"><span id="logo"></span></div>
+            <div class="col align-items-center flex-col">
+                <span><strong>Ass. Escalanque</strong></span>
+                <span>14 rue des calanques</span>
+                <span>13009 Marseille</span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
